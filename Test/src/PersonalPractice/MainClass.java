06cbf4d2a8703cc772d3b2043496f9990d00507c
Lara4lani <laramisiaforlani@gmail.com>
2022-11-13 21:09:17 +0100

    deleted massimo minimo method

--- Test/src/PersonalPractice/MainClass.java
+++ Test/src/PersonalPractice/MainClass.java
@@ -4,6 +4,14 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Methods pers;
+		pers = new Methods();
+		
+		//pers.Triangle(7);
+		
+		pers.massimoMinimo();
+		
 
 	}
 
--- Test/src/PersonalPractice/Methods.java
+++ Test/src/PersonalPractice/Methods.java
@@ -1,5 +1,54 @@
 package PersonalPractice;
 
+import java.util.Scanner;
+
 public class Methods {
 
+	Scanner input = new Scanner(System.in);
+	
+	static void Triangle(int asterisks){
+
+        for( int y=0 ; y<asterisks ; y++ ){
+
+            for( int x=0 ; x<=y ; x++ ){
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+        }
+   
+    }
+    
+    
+    public int scegliNumero(String s)
+    {
+    	System.out.println(s);
+    	return input.nextInt();
+    }
+    
+    public void massimoMinimo() 
+    {
+    	int numeri = 0;
+    	String scelta;
+    	int max=numeri;
+		int min=numeri;
+    	
+    	do
+    	{
+    		numeri=scegliNumero("scegli un numero");
+    		System.out.println("vuoi continuare? y o n");
+    		scelta=input.next();
+    		if(numeri<min)
+    		{
+    			min=numeri;
+    		}
+    	
+    	}while(scelta.equals("y"));
+    	System.out.println("Il valore massimo e' " + max + " e il valore minimo e' " + min);
+    }
+
+	
 }
